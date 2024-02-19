e42c958d45ae8ba95ff696101dd5909ef00b37de
Andres Herrera <aherrera@bevertec.com>
2024-02-18 23:59:53 -0500

    chore: enhance CheckedFunction description

--- src/main/java/io/github/aherreraz/util/function/CheckedFunction.java
+++ src/main/java/io/github/aherreraz/util/function/CheckedFunction.java
@@ -1,7 +1,9 @@
 package io.github.aherreraz.util.function;
 
 /**
- * Represents a function that accepts one argument and produces a result
+ * Represents a function that accepts one argument and produces a result. Similar to a
+ * {@link java.util.function.Function Function}, but also works if a checked exception is declared
+ *
  * @param <T> the type of the input to the function
  * @param <R> the type of the result of the function
  * @param <E> the type of the exception that the function can throw
@@ -10,6 +12,7 @@ package io.github.aherreraz.util.function;
 public interface CheckedFunction<T, R, E extends Exception> {
     /**
      * Applies the function to the given argument
+     *
      * @param t the function argument
      * @return the function result
      * @throws E the exception that can be thrown
