438a2259353be844080623d3244223c7bf5337e6
Andres Herrera <aherrera@bevertec.com>
2024-02-19 02:17:23 -0500

    chore: add description to CheckedRunnable

--- src/main/java/io/github/aherreraz/util/function/CheckedRunnable.java
+++ src/main/java/io/github/aherreraz/util/function/CheckedRunnable.java
@@ -1,7 +1,14 @@
 package io.github.aherreraz.util.function;
 
-
+/**
+ * Represents an operation that takes no arguments and returns no result.
+ * @param <E> the type of the exception that can be thrown
+ */
 @FunctionalInterface
 public interface CheckedRunnable<E extends Exception> {
+    /**
+     * Performs this operation
+     * @throws E the exception that can be thrown
+     */
     void run() throws E;
 }
